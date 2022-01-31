c3bd6a9e5c8a9430f67373e04b2d3c74d18a7d44
andrehsym <kochetkovandre1993@gmail.com>
2022-01-31 13:46:41 +0300

    1 commit

---
+++ src/main/java/pro/sky/java/homeworks/course2/coursework/Course2CourseworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.homeworks.course2.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2CourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2CourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/homeworks/course2/coursework/Course2CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.homeworks.course2.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2CourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
